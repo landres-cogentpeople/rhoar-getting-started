0a0f620586c5647825205d73c63cd15c9ddaaee2
A8PU <leo.andres_sc@sparksoftcorp.com>
2023-01-27 10:38:48 -0500

    Add FruitController.java file.

---
+++ spring/spring-rhoar-intro/src/main/java/com/example/service/FruitController.java
@@ -0,0 +1,34 @@
+package com.example.service;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/fruits")
+public class FruitController {
+
+    private final FruitRepository repository;
+
+    public FruitController(FruitRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Fruit> getAll() {
+        return this.repository.findAll();
+    }
+
+    // TODO: Add additional service calls here
+}
