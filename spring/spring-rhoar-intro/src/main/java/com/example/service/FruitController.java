9d972ecc45979bd6f5b4ac5798569c3b14e1dcee
A8PU <leo.andres_sc@sparksoftcorp.com>
2023-01-27 11:06:40 -0500

    Make updates to FruitController.java to complete requests.

--- spring/spring-rhoar-intro/src/main/java/com/example/service/FruitController.java
+++ spring/spring-rhoar-intro/src/main/java/com/example/service/FruitController.java
@@ -31,4 +31,52 @@ public class FruitController {
     }
 
     // TODO: Add additional service calls here
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Fruit post(@RequestBody(required = false) Fruit fruit) {
+        verifyCorrectPayload(fruit);
+
+        return this.repository.save(fruit);
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Fruit get(@PathVariable("id") Integer id) {
+        verifyFruitExists(id);
+
+        return this.repository.findById(id).orElse(null);
+    }
+
+    @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Fruit put(@PathVariable("id") Integer id, @RequestBody(required = false) Fruit fruit) {
+        verifyFruitExists(id);
+        verifyCorrectPayload(fruit);
+
+        fruit.setId(id);
+        return this.repository.save(fruit);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        verifyFruitExists(id);
+
+        this.repository.deleteById(id);
+    }
+
+    private void verifyFruitExists(Integer id) {
+        if (!this.repository.existsById(id)) {
+            throw new RuntimeException(String.format("Fruit with id=%d was not found", id));
+        }
+    }
+
+    private void verifyCorrectPayload(Fruit fruit) {
+        if (Objects.isNull(fruit)) {
+            throw new RuntimeException("Fruit cannot be null");
+        }
+
+        if (!Objects.isNull(fruit.getId())) {
+            throw new RuntimeException("Id field must be generated");
+        }
+    }
 }
